4417eeb72b8ba9b4f42271ce9c3349251dc7f10f
Ben <benjamin.s.record@gmail.com>
2021-10-13 23:05:14 -0600

    new path

--- .config/ben/installs/00-ssh-key.sh
+++ .config/ben/installs/00-ssh-key.sh
@@ -4,7 +4,7 @@ set e
 echo "Installing OpenSSH..."
 sudo apt install -y openssh-client openssh-server 1> /dev/null
 
-ssh-keygen -q -t ed25519 -a 4096 -P "" -f /home/ben/.ssh/id_ed25519
+ssh-keygen -q -t ed25519 -a 4096 -P "" -f $HOME/.ssh/id_ed25519
 
 echo "\nPublic SSH Key:"
 cat ~/.ssh/id_ed25519.pub
