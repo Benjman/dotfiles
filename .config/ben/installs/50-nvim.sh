c11e2ed2acd5961d2a52cdc4dea47470f5afc110
Ben <benjamin.s.record@gmail.com>
2021-10-22 18:31:12 -0600

    uprade to v0.5.1

--- .config/ben/installs/50-nvim.sh
+++ .config/ben/installs/50-nvim.sh
@@ -1,33 +1,34 @@
 #!/usr/bin/env bash
 set -e
 
-sudo apt install -y \
-    autoconf \
-    automake \
-    cmake \
-    make \
-    curl \
-    g++ \
-    gettext \
-    libtool \
-    libtool-bin \
-    ninja-build \
-    pkg-config \
-    python \
-    python3 \
-    python3-pip \
-    unzip \
-    clangd \
-    llvm \
-    ripgrep \
-    fd-find
+#sudo apt install -y \
+#    autoconf \
+#    automake \
+#    cmake \
+#    make \
+#    curl \
+#    g++ \
+#    gettext \
+#    libtool \
+#    libtool-bin \
+#    ninja-build \
+#    pkg-config \
+#    python \
+#    python3 \
+#    python3-pip \
+#    unzip \
+#    clangd \
+#    llvm \
+#    ripgrep \
+#    fd-find
 
-DIR=$HOME/.local/lib/neovim
-BRANCH=release-0.5
+[ -d /tmp/neovim ] && rm -r /tmp/neovim
+
+DIR=/tmp/neovim
+BRANCH=v0.5.1
 REPO=https://github.com/neovim/neovim.git
 Z_PWD=$PWD
 
-[ -d $DIR ] && echo "$DIR already exists. Aborting installation." && exit
 mkdir -p $DIR
 git clone --branch $BRANCH $REPO $DIR
 
@@ -35,6 +36,6 @@ cd $DIR
 make CMAKE_BUILD_TYPE=Release
 sudo make install
 
-python3 -m pip install pynvim
-
 nvim --version
+
+cd $Z_PWD
