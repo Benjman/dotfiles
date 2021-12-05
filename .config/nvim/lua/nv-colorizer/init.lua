cc34eef5e4f55f9acabe1b09a6cde869f35e5139
Ben <benjamin.s.record@gmail.com>
2021-12-05 11:36:49 -0700

    disable names

--- .config/nvim/lua/nv-colorizer/init.lua
+++ .config/nvim/lua/nv-colorizer/init.lua
@@ -1,15 +1,17 @@
-require'colorizer'.setup()
-
-DEFAULT_OPTIONS = {
-  RGB      = true;        -- #RGB hex codes
-  RRGGBB   = true;        -- #RRGGBB hex codes
-  names    = true;        -- "Name" codes like Blue
-  RRGGBBAA = true;        -- #RRGGBBAA hex codes
-  rgb_fn   = true;        -- CSS rgb() and rgba() functions
-  hsl_fn   = true;        -- CSS hsl() and hsla() functions
-  css      = true;        -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-  css_fn   = true;        -- Enable all CSS *functions*: rgb_fn, hsl_fn
-  -- Available modes: foreground, background
-  mode     = 'background';    -- Set the display mode.
+local configs = {
+	RGB      = true;         -- #RGB hex codes
+	RRGGBB   = true;         -- #RRGGBB hex codes
+	names    = false;        -- "Name" codes like Blue
+	RRGGBBAA = true;         -- #RRGGBBAA hex codes
+	rgb_fn   = true;         -- CSS rgb() and rgba() functions
+	hsl_fn   = true;         -- CSS hsl() and hsla() functions
+	css      = false;        -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+	css_fn   = true;         -- Enable all CSS *functions*: rgb_fn, hsl_fn
+	mode     = 'background'; -- Set the display mode.
 }
 
+local filetypes = {
+  "*"
+}
+
+require('colorizer').setup(filetypes, configs)
