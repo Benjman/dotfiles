c50d10cccecc1cdda102850751780411570791a9
Ben <benjamin.s.record@gmail.com>
2021-12-05 05:57:38 -0700

    lua lsp

--- .config/nvim/lua/nv-lsp/init.lua
+++ .config/nvim/lua/nv-lsp/init.lua
@@ -1,8 +1,9 @@
 vim.cmd([[
     augroup lsp
         autocmd! lsp
-        autocmd FileType java :lua require('nv-lsp.jdt').start()
+        autocmd FileType cmake :lua require('lspconfig').cmake.setup()
         autocmd FileType cpp :lua require('nv-lsp.clangd')
-        autocmd FileType cmake :lua require('lspconfig').cmake.setup{}
+        autocmd FileType java :lua require('nv-lsp.jdt').start()
+        autocmd FileType lua :lua require('nv-lsp.lua')
     augroup END
 ]])
