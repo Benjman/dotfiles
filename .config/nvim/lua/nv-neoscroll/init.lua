2863f66e6f30d16cbc40bb18cc0f5f66d05f48f2
Ben <benjamin.s.record@gmail.com>
2021-12-05 12:30:46 -0700

    updating mappings

--- .config/nvim/lua/nv-neoscroll/init.lua
+++ .config/nvim/lua/nv-neoscroll/init.lua
@@ -1,24 +1,20 @@
-local neoscroll = require('neoscroll')
-
-neoscroll.setup({
+require('neoscroll').setup({
   -- All these keys will be mapped. Pass an empty table ({}) for no mappings
-  mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+  mappings = {'<C-b>', '<C-f>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
   hide_cursor = true,          -- Hide cursor while scrolling
   stop_eof = true,             -- Stop at <EOF> when scrolling downwards
   respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
   cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
 })
 
-
-local opt = {silent = true, noremap = true}
-local map = vim.api.nvim_set_keymap
-
-map('n', '<C-u>', '<cmd>lua require(\'neoscroll\').scroll(-vim.wo.scroll, true, 200)<cr>', opt)
-map('n', '<C-d>', '<cmd>lua require(\'neoscroll\').scroll( vim.wo.scroll, true, 200)<cr>', opt)
-map('n', '<C-b>', '<cmd>lua require(\'neoscroll\').scroll(-vim.api.nvim_win_get_height(0), true, 300)<cr>', opt)
-map('n', '<C-f>', '<cmd>lua require(\'neoscroll\').scroll( vim.api.nvim_win_get_height(0), true, 300)<cr>', opt)
-map('n', '<C-y>', '<cmd>lua require(\'neoscroll\').scroll(-0.10, false, 100)<cr>', opt)
-map('n', '<C-e>', '<cmd>lua require(\'neoscroll\').scroll( 0.10, false, 100)<cr>', opt)
-map('n', 'zt', '<cmd>lua require(\'neoscroll\').zt(200)<cr>', opt)
-map('n', 'zz', '<cmd>lua require(\'neoscroll\').zz(200)<cr>', opt)
-map('n', 'zb', '<cmd>lua require(\'neoscroll\').zb(200)<cr>', opt)
+require('neoscroll.config').set_mappings({
+  ['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '200'}},
+  ['<C-d>'] = {'scroll', {'vim.wo.scroll',  'true', '200'}},
+  ['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)',  'true', '450'}},
+  ['<C-f>'] = {'scroll', {'vim.api.nvim_win_get_height(0)',  'true', '450'}},
+  ['<C-y>'] = {'scroll', {'-0.10',  'false', '100'}},
+  ['<C-e>'] = {'scroll', {'0.10',  'false', '100'}},
+  ['zt']    = {'zt', {'250'}},
+  ['zz']    = {'zz', {'250'}},
+  ['zb']    = {'zb', {'250'}},
+})
