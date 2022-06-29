75a64adb7ca6321bb75340e9d7b21dfed9dc4e78
Ben <benjamin.s.record@gmail.com>
2022-06-29 15:22:47 -0600

    including slock

--- slock/.local/lib/slock/config.h
+++ slock/.local/lib/slock/config.h
@@ -1,15 +1,15 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *user  = "ben";
+static const char *group = "ben";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#001133",   /* during input */
+	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+static const int failonclear = 0;
 
 /* time in seconds before the monitor shuts down */
 static const int monitortime = 5;
