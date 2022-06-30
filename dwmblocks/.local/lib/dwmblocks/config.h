803799af9398495170b6433281f3f1db5fcdb506
Ben <benjamin.s.record@gmail.com>
2022-06-29 19:44:24 -0600

    dwmblocks modules

--- dwmblocks/.local/lib/dwmblocks/config.h
+++ dwmblocks/.local/lib/dwmblocks/config.h
@@ -2,9 +2,11 @@ static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
     {"", "sb-memory", 10, 14},
     {"", "sb-cpu", 10, 18},
-    {"", "sb-battery", 5, 3},
+    {"", "sb-battery", 300, 3},
     {"", "sb-internet", 5, 4},
-    {"", "sb-mouse", 60, 5},
+    {"", "sb-mouse", 300, 5},
+    {"", "sb-headphones", 300, 3},
+    {"", "sb-weather", 900, 1},
     {"", "sb-clock", 1, 1},
 };
 
