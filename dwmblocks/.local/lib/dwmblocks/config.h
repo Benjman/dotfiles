f0bc298dcec1c9ebcdacbe7f04e4e0742f6500e7
Ben <benjamin.s.record@gmail.com>
2022-06-30 15:51:07 -0600

    more blocks modules

--- dwmblocks/.local/lib/dwmblocks/config.h
+++ dwmblocks/.local/lib/dwmblocks/config.h
@@ -1,13 +1,21 @@
+/**
+ * Signals can be sent to the dwmblocks process to individual scripts, by either the pkill with
+ * the RTMIN offset, or the kill command by adding 34 as the offset:
+ *      pkill   -RTMIN+8    $(pidof dwmblocks)      # this would signal the weather module
+ *      kill    -43         $(pidof dwmblocks)      # this would signal the volume module
+ **/
+
 static const Block blocks[] = {
-    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-    {"", "sb-memory", 10, 14},
-    {"", "sb-cpu", 10, 18},
-    {"", "sb-battery", 300, 3},
-    {"", "sb-internet", 5, 4},
-    {"", "sb-mouse", 300, 5},
-    {"", "sb-headphones", 300, 3},
-    {"", "sb-weather", 900, 1},
-    {"", "sb-clock", 1, 1},
+  /* icon   command             update interval     signal */
+    {"",    "sb-memory",        10,                 6},
+    {"",    "sb-cpu",           10,                 3},
+    {"",    "sb-battery",       300,                1},
+    {"",    "sb-internet",      5,                  5},
+    {"",    "sb-mouse",         300,                7},
+    {"",    "sb-headphones",    300,                4},
+    {"",    "sb-volume",        0,                  10},
+    {"",    "sb-weather",       900,                8},
+    {"",    "sb-clock",         1,                  2},
 };
 
-static char *delim = " | ";
+static const char *delim = " | ";
