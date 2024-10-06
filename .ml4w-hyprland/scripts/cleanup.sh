3fd2f0aac2b772d5a6835102b66e9398968ba585
Ben Record <benjamin.s.record@gmail.com>
2024-10-05 22:36:15 -0600

    npm files to xdg dir

--- .ml4w-hyprland/scripts/cleanup.sh
+++ .ml4w-hyprland/scripts/cleanup.sh
@@ -10,3 +10,18 @@ if [ -d ~/go ]; then
     sleep 2
   fi
 fi
+
+if [ -d ~/.npm ]; then
+  if [ -n "$NPM_CONFIG_USERCONFIG" ]; then
+    npm_dir=$(eval "echo $(grep prefix $NPM_CONFIG_USERCONFIG | awk -F"=" '{print $2}')")
+    if [ -n "$npm_dir" ]; then
+      mv ~/.npm $npm_dir
+    else
+      echo "ERROR: Failed to move ~/npm: Could not find prefix directory"
+      sleep 2
+    fi
+  else
+    echo "ERROR: Failed to move ~/.npm: \$NPM_CONFIG_USERCONFIG not set"
+    sleep 2
+  fi
+fi
