f3d620414ebc2307cda58cd1e972ecc5ba08771a
Ben Record <benjamin.s.record@gmail.com>
2024-10-17 19:10:08 +0000

    go files to xdg dir

--- .ml4w-hyprland/scripts/cleanup.sh
+++ .ml4w-hyprland/scripts/cleanup.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env /usr/bin/bash
 
 [[ -f ~/.wget-hsts ]] && rm ~/.wget-hsts
+
+if [ -d ~/go ]; then
+  if [ -n "$GOPATH" ]; then
+    mv ~/go $GOPATH
+  else
+    echo "ERROR: Failed to move ~/go: \$GOPATH not set"
+    sleep 2
+  fi
+fi
